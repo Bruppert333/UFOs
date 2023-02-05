5cfe5d0a8decfdf4f79e9c3960881cc46a581703
Bryce <bryceruppert@gmail.com>
2023-02-05 14:04:09 -0700

    Challenge

--- Starter_Code/web/static/js/app.js
+++ Starter_Code/web/static/js/app.js
@@ -30,20 +30,21 @@ var filters = {};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-  let changeElement = d3.select(this);
+  let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-  let changeValue = changeElement.property("value");
+  let changeValue = changedElement.property("value");
+  console.log(changeValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
-  let changeID = changeElement.attr("id");
-  
+  let filterId = changedElement.attr("id");
+  console.log(filterId);
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
   if (changeValue) {
-    filters[changeID] = changeValue;
+    filters[filterId] = changeValue;
   }
   else {
-    delete filters[changeID];
+    delete filters[filterId];
   }
   
     // 6. Call function to apply all filters and rebuild the table
@@ -59,19 +60,16 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    for (const [filter_Id, value] of Object.entries(filters)) {
-      console.log(`${filter_Id}: ${value}`);
-      if (value) {
-        filteredData = filteredData.filter(row => row[filter_Id] === value);
-      }
-    }
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData =  filteredData.filter(row => row[key]=== value);
+    }); 
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("#filter-btn").on("change", updateFilters);
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
